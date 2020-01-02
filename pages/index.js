92c18bb1b75d4c761801aba5ae7b98873aa4bc7a
Best <irb.ossystem@gmail.com>
2020-01-02 20:27:45 +0200

    Import fixes

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Nav from "../components/nav";
 import Carousel from "react-bootstrap/Carousel";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
-import Col from "npm/Col";
+import Col from "react-bootstrap/Col";
 import Paper from "@material-ui/core/Paper";
 import { Typography } from "@material-ui/core";
 
